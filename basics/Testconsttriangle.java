38377c75420d73df406e04ec074ef6031ff24353
nidhimishra <mnidhi2999@gmail.com>
2023-01-25 15:31:12 +0530

    nidhi

---
+++ basics/Testconsttriangle.java
@@ -0,0 +1,14 @@
+package in.co.basics;
+
+public class Testconsttriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConstTriangle ct=new ConstTriangle(23, 34);
+		int y=ct.getbase();
+		int u=ct.getheight();
+				System.out.println(u*y);
+
+	}
+
+}
